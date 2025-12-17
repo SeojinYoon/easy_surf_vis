daca28904c2aea212336dc7af49874b992ad2dd7
seojinyoon <dbstjwls1234@hanmail.net>
2025-12-17 10:11:20 +0900

    dev*

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "easysurfvis",
-    version = "0.0.1dev7",
+    version = "0.0.1dev8",
     author = "seojin",
     author_email = "pures1@hanyang.ac.kr",
     description = "visualize surface map easily",
--- src/easysurfvis/surface_visualization.py
+++ src/easysurfvis/surface_visualization.py
@@ -15,10 +15,10 @@ import plotly.graph_objects as go
 from IPython.display import HTML
 
 # Custom Libraries
-from cores.surface_data import surf_paths, map_2d_to3d
-from cores.surface_roi import show_sulcus
-from cores.surface_util import mean_datas
-from cores.custom_matplotlib import make_colorbar
+from easysurfvis.cores.surface_data import surf_paths, map_2d_to3d
+from easysurfvis.cores.surface_roi import show_sulcus
+from easysurfvis.cores.surface_util import mean_datas
+from easysurfvis.cores.custom_matplotlib import make_colorbar
 
 # Functions
 def draw_surf_roi(roi_value_array, 
