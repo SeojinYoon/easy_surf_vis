c2b1681448d7970c24b3c17d3c2c1b493beea3d2
seojinyoon <dbstjwls1234@hanmail.net>
2026-06-02 01:29:07 +0900

    commit for maintaining trusted publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "easysurfvis",
-    version = "1.0.2",
+    version = "1.0.3",
     author = "seojin",
     author_email = "pures1@hanyang.ac.kr",
     description = "visualize surface map easily",
