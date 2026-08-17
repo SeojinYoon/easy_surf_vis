2892fd44b870ba902f3ba4fd2f0e8a8c01cfa899
seojinyoon <dbstjwls1234@hanmail.net>
2026-08-17 19:26:19 +0900

    v1.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "easysurfvis",
-    version = "1.0.4",
+    version = "1.0.6",
     author = "seojin",
     author_email = "pures1@hanyang.ac.kr",
     description = "visualize surface map easily",
